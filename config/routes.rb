a397470ba80815718fbe4f6bc84bdb8c13753b80
Blaine Pace <blainepace@gmail.com>
2016-12-02 16:07:17 -0800

    Change root route to articles#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'welcome#home'
 
-  root to: 'welcome#home'
+  #root to: 'welcome#home'
+  root to: 'articles#index'
 
   resources :articles
 end
