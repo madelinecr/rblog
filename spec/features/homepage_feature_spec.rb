369604054d9f570be9da97534453cc540b9a75df
Blaine Pace <blainepace@gmail.com>
2016-12-08 15:47:46 -0800

    Top bar moved to partial, links based on user
    Register only rendered if there is no admin in the system, sign in / sign out
    show based on user's login status.

--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -7,8 +7,25 @@ describe "Home page", :type => :feature do
   end
 
   specify { expect(page).to have_content("Sign In") }
-  specify { expect(page).to have_content("Sign Out") }
   specify { expect(page).to have_content("Register") }
   specify { expect(page).to have_content("About") }
   specify { expect(page).to have_content("Articles") }
+
+  describe "signed in" do
+    before :each do
+      login_admin(create(:admin))
+      visit root_path
+    end
+
+    specify { expect(page).to have_content("Sign Out") }
+  end
+
+  describe "signed out, user exists" do
+    before :each do
+      create(:admin)
+      visit current_path
+    end
+
+    specify { expect(page).to have_no_content("Register") }
+  end
 end
