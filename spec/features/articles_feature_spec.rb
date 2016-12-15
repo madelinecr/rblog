d27309956707888199bf2772c7d46f9ac015f29f
Blaine Pace <blainepace@gmail.com>
2016-12-15 13:33:06 -0800

    Add 'Add Article' button to articles#index

--- spec/features/articles_feature_spec.rb
+++ spec/features/articles_feature_spec.rb
@@ -2,32 +2,41 @@ require 'rails_helper'
 
 describe "Articles", :type => :feature do
 
-  before :each do
-    login_admin(create(:admin))
-  end
-
-  describe "#new" do
-    it "creates a new article" do
-      visit "/articles/new"
-      within("#form") do
-        fill_in "Title", with: "Lorem ipsum dolor sit amet"
-        fill_in "Body",  with: "consectetur adipiscing elit"
-      end
-      click_button 'Create'
-      expect(page).to have_content("Success")
+  describe "logged in as admin" do
+    before :each do
+      login_admin(create(:admin))
     end
-  end
 
-  describe "#edit" do
-    it "edits an article" do
-      article = create(:article)
-      visit edit_article_path(article.id)
-      within("#form") do
-        fill_in "Title", with: "Edited title"
-        fill_in "Body",  with: "Edited body"
+    describe "#index" do
+      it "has a new article link" do
+        visit articles_path
+        expect(page).to have_content("New Article")
       end
-      click_button 'Update'
-      expect(page).to have_content("Success")
+    end
+
+    describe "#new" do
+      it "creates a new article" do
+        visit new_article_path
+        within("#form") do
+          fill_in "Title", with: "Lorem ipsum dolor sit amet"
+          fill_in "Body",  with: "consectetur adipiscing elit"
+        end
+        click_button 'Create'
+        expect(page).to have_content("Success")
+      end
+    end
+
+    describe "#edit" do
+      it "edits an article" do
+        article = create(:article)
+        visit edit_article_path(article.id)
+        within("#form") do
+          fill_in "Title", with: "Edited title"
+          fill_in "Body",  with: "Edited body"
+        end
+        click_button 'Update'
+        expect(page).to have_content("Success")
+      end
     end
   end
 end
