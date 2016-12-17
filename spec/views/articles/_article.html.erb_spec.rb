2e43d38548eea1083cfdcf1e2e1d52ede9a69db5
Blaine Pace <blainepace@gmail.com>
2016-12-17 14:26:28 -0800

    Change syntax on view spec slightly

--- spec/views/articles/_article.html.erb_spec.rb
+++ spec/views/articles/_article.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "articles/_article" do
-  before :each do
-    @article = create(:article)
-    assign(:article, @article)
-    render @article
-  end
+RSpec.describe "articles/_article", type: :view do
+  describe "signed out" do
+    before :each do
+      @article = create(:article)
+      assign(:article, @article)
+      render @article
+    end
 
-  specify { expect(rendered).to have_content(@article.title) }
-  specify { expect(rendered).to have_content(@article.body) }
-  specify { expect(rendered).to have_content("Edit") }
-  specify { expect(rendered).to have_content("Delete") }
+    specify { expect(rendered).to have_content(@article.title) }
+    specify { expect(rendered).to have_content(@article.body) }
+  end
 end
