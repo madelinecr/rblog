fe0581a71f02490676ae2828ac545021c5e42431
Blaine Pace <blainepace@gmail.com>
2016-12-20 13:25:02 -0800

    Made some tweaks to allow paperclip to function

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ApplicationRecord
   validates :title, length: { minimum: 8 }
 
   has_attached_file :header, styles: { medium: "300x300>" }
+  validates_attachment_content_type :header, :content_type => ["image/png"]
 
   def body_markdown
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
