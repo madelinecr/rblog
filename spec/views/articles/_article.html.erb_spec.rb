e172e99a0c660bcb497675042aa7d1f1f3877148
Blaine Pace <blainepace@gmail.com>
2016-12-28 16:18:34 -0800

    Remove unnecessary describe block

--- spec/views/articles/_article.html.erb_spec.rb
+++ spec/views/articles/_article.html.erb_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "articles/_article", type: :view do
-  describe "signed out" do
-    before :each do
-      @article = create(:article)
-      assign(:article, @article)
-      render @article
-    end
-
-    specify { expect(rendered).to have_content(@article.title) }
-    specify { expect(rendered).to have_content(@article.body) }
+  before :each do
+    @article = create(:article)
+    assign(:article, @article)
+    render @article
   end
+
+  specify { expect(rendered).to have_content(@article.title) }
+  specify { expect(rendered).to have_content(@article.body) }
 end
