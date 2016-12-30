10a4beeb4a75b215777c150493bb73eba48f472a
Blaine Pace <blainepace@gmail.com>
2016-12-29 19:39:56 -0800

    Create article_with_header factory for paperclip
    Paperclip attachment header-containing articles now able
    to be instantiated via factorygirl.
    
    Syntax updated in factories.rb to be more modern.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,18 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
-  factory :admin do |f|
-    f.email                 "testuser@example.org"
-    f.password              "password"
-    f.password_confirmation "password"
+  factory :admin do
+    email                 "testuser@example.org"
+    password              "password"
+    password_confirmation "password"
   end
 
-  factory :article do |f|
-    f.title "Lorem ipsum dolor sit amet"
-    f.body "consectetur adipiscing elit"
+  factory :article do
+    title "Lorem ipsum dolor sit amet"
+    body "consectetur adipiscing elit"
+
+    factory :article_with_header do
+      header { fixture_file_upload(Rails.root.join('spec', 'factory_image.png'), 'image/png') }
+    end
   end
 end
