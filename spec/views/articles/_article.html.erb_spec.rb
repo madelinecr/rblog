16aae24e547880d771f90d6e98735e936e6a5c76
Blaine Pace <blainepace@gmail.com>
2016-12-29 19:42:06 -0800

    Article header only rendered if uploaded

--- spec/views/articles/_article.html.erb_spec.rb
+++ spec/views/articles/_article.html.erb_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "articles/_article", type: :view do
-  before :each do
-    @article = create(:article)
-    assign(:article, @article)
-    render @article
+  describe "no header" do
+    before :each do
+      @article = create(:article)
+      assign(:article, @article)
+      render @article
+    end
+
+    specify { expect(rendered).to have_content(@article.title) }
+    specify { expect(rendered).to have_content(@article.body) }
+    specify { expect(rendered).to_not have_xpath("//img[@src='#{@article.header.url(:standard)}']") }
   end
 
-  specify { expect(rendered).to have_content(@article.title) }
-  specify { expect(rendered).to have_content(@article.body) }
-  specify { expect(rendered).to have_xpath("//img[@src='#{@article.header.url(:standard)}']") }
+  describe "header" do
+    before :each do
+      @article = create(:article_with_header)
+      assign(:article, @article)
+      render @article
+    end
+
+    specify { expect(rendered).to have_xpath("//img[@src='#{@article.header.url(:standard)}']") }
+  end
 end
