8f480723fa4799ac617de2b90565168a365ed3a9
Blaine Pace <blainepace@gmail.com>
2017-01-19 12:46:32 -0800

    Rename photo title to caption
    This keeps the article nested form from throwing a duplicate field
    error.

--- db/migrate/20161230203118_create_photos.rb
+++ db/migrate/20161230203118_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
-      t.string :title
+      t.string :caption
       t.belongs_to :article, index: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20161230203118) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.string   "title"
+    t.string   "caption"
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
